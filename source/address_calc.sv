4755d972bc2ba0b1eafaec9915ab9276dc1b5fb9
Abhishek Srikanth <srikanth>
2015-11-28 00:10:45 -0500

    Added modules to address calculator

--- source/address_calc.sv
+++ source/address_calc.sv
@@ -8,15 +8,48 @@
 
 module address_calc
 (
-	input wire [12:0] width,
-	input wire [12:0] height,
+	input wire clk,
+	input wire n_rst,
+
+	input wire sram_mode,
+	input wire sdram_mode,
+
+	input wire update,
+	input wire start_flag,
+	input wire [12:0] image_width,
+
 	input wire [25:0] start_address_sdram,
 	input wire [25:0] finish_address_sdram,
-	input wire [2:0] mode,
+	input wire [25:0] rowCache_address_sram,
+	input wire [25:0] output_address_sram,
+
 	output reg [25:0] sdram_address,
 	output reg [25:0] sram_address
 );
 
+sdram_address_calc sdramAddrCalc
+(
+	.clk					(clk),
+	.n_rst					(n_rst),
+	.mode					(sdram_mode),
+	.enable					(enable),
+	.load					(start_flag),
+	.start_address			(start_address_sdram),
+	.finish_address			(finish_address_sdram),
+	.sdram_address			(sdram_address)
+);
 
+sram_address_calc sramAddrCalc
+(
+	.clk					(clk),
+	.n_rst					(n_rst),
+	.mode					(sram_mode),
+	.enable					(enable),
+	.clear					(start_flag),
+	.image_width			(image_width),
+	.sram_rowCacheStart		(rowCache_address_sram),
+	.sram_outputAddrStart	(output_address_sram),
+	.sram_addr				(sram_address)
+);
 
 endmodule
