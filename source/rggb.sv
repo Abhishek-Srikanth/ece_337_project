ba039fe70d1ed3c0101d6a375917b41174dc7987
Tiger Cheng <Swaedo@users.noreply.github.com>
2015-12-13 22:45:49 -0500

    rggb output ports change. added safety parens

--- source/rggb.sv
+++ source/rggb.sv
@@ -6,27 +6,23 @@ module rggb
    input wire [7:0] wb_4,
    input wire row,
    input wire col,
-   output reg [31:0] out
+   output reg [7:0] R,
+   output reg [7:0] G1,
+   output reg [7:0] G2,
+   output reg [7:0] B
    );
 
-   reg R;
-   reg G1;
-   reg G2;
-   reg B;
-   
-   assign out = {R, G1, G2, B};
-   
    always_comb
-    begin: MUX
+    begin: RGGB_MUX
 	{R, G1, G2, B} = 'x;
 	
-	if(row == 1'b0 && col == 1'b0)
+	if((row == 1'b0) && (col == 1'b0))
 	     {R, G1, G2, B} = {wb_1, wb_2, wb_3, wb_4};
-	if(row == 1'b0 && col == 1'b1)
+	if((row == 1'b0) && (col == 1'b1))
 	     {G1, R, B, G2} = {wb_1, wb_2, wb_3, wb_4};
-	if(row == 1'b1 && col == 1'b0)
+	if((row == 1'b1) && (col == 1'b0))
 	     {G1, B, R, G2} = {wb_1, wb_2, wb_3, wb_4};
-	if(row == 1'b1 && col == 1'b1)
+	if((row == 1'b1) && (col == 1'b1))
 	     {B, G1, G2, R} = {wb_1, wb_2, wb_3, wb_4};
     end // block: MUX
 
