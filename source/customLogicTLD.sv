09bd4f8b5c3fffa432f352814712ae4fe769ea85
Abhishek-Srikanth <srikanth@purdue.edu>
2015-12-14 15:43:10 -0500

    mostly compelete wiring TL. need a delay module

--- source/customLogicTLD.sv
+++ source/customLogicTLD.sv
@@ -8,158 +8,186 @@
 
 module customLogicTLD
 (
+	input wire clk,
+	input wire n_rst,
+	// control register values
+	input wire startControlRegister,
+	input wire [12:0] imageWidth,
+	input wire [12:0] imageHeight,
+	input wire [25:0] start_addr_sdram,
+	input wire [25:0] finish_addr_sdram,
+	input wire [1:0] filterMode,
+	input wire [7:0] betaValue,
+	input wire sdram_datareadvalid,
 
+	// sdram MO-SI wires
+	output wire sdram_read_en,
+	output wire sdram_write_en,
+	output wire [25:0] address_sdram,
+	output wire finish_flag
 );
+
+wire start_flag;
+wire rollover_i, rollover_i_wr, rollover_j;					// rollover wires
+wire [7:0] wb1, [7:0] wb2, [7:0] wb3, [7:0] wb4;			// windowBuffer connecting wires
+wire [31:0] reorderedWire;									// reOrdered wire to filter top level
+wire [31:0] postFilterData;									// output of filter block
+wire [12:0] i_value, [12:0]j_value;							// value of counter i and j
+wire i_wr_en, i_en, j_en;  									// enable wires
+wire addrCalc_sram_en, addrCalc_sdram_en, sram_en, WB_en;	// enable wires
+wire sram_datareadvalid;
+wire addrCalc_sram_mode, addrCalc_sdram_mode;				// addrCalc mode
+wire [25:0] address_sram;									// address to query sram
+wire [2:0] mode_WB, mode_sram;								// sram and WB operation modes
 
 // rising edge detector
 risingEdgeDetector edgeDetector
 (
-	.clk					(),
-	.startData				(),
+	.clk					(clk),
+	.startData				(startControlRegister),
 
-	.start_flag				()
+	.start_flag				(start_flag)
 );
 
 
 // i_wr port mapping
 i_wr_counter outputColCounter
 (
-	.clk					(),
-	.clear					(),
-	.rollover_val			(),	// 12:0
-	.count_enable			(),
+	.clk					(clk),
+	.clear					(start_flag),
+	.rollover_val			(imageWidth),	// 12:0
+	.count_enable			(i_wr_en),
 	
-	.rollover_flag			(),
-	.value					()	// 12:0
+	.rollover_flag			(rollover_i_wr),
+	//.value				()				// 12:0	// not used
 );
 
 // i port mapping
 i_col_counter colCounter
 (
-	.clk					(), 
-	.clear					(),
-	.rollover_val			(),	// 12:0
-	.count_enable			(),
+	.clk					(clk), 
+	.clear					(start_flag),
+	.rollover_val			(imageWidth),	// 12:0
+	.count_enable			(i_en),
 
-	.rollover_flag			(),
-	.value					()	// 12:0
-);
+	.rollover_flag			(rollover_i),
+	.value					(i_value)		// 12:0	
+)
 
 // j port mapping
 j_row_counter rowCounter
 (
-	.clk					(), 
-	.clear					(),
-	.rollover_val			(),	// 12:0
-	.count_enable			(),
+	.clk					(clk), 
+	.clear					(start_flag),
+	.rollover_val			(imageHeight),	// 12:0
+	.count_enable			(j_en),
 
-	.rollover_flag			(),
-	.value					()	// 12:0
+	.rollover_flag			(rollover_j),
+	.value					(j_value)		// 12:0	
 );
 
 // addrCalc module
 address_calc	addressCalculator
 (
-	.clk					(),
-	.n_rst					(),
-	.sram_mode				(),
-	.sdram_mode				(),
-	.sram_update			(),
-	.sdram_update			(),
-	.start_flag				(),
-	.image_width			(), // [12:0] 
-	.start_address_sdram	(),	// 25:0
-	.finish_address_sdram	(),	// 25:0
+	.clk					(clk),
+	.n_rst					(n_rst),
+	.sram_mode				(addrCalc_sram_mode),
+	.sdram_mode				(addrCalc_sdram_mode),
+	.sram_update			(addrCalc_sram_en),
+	.sdram_update			(addrCalc_sdram_en),
+	.start_flag				(start_flag),
+	.image_width			(imageWidth), // [12:0] 
+	.start_address_sdram	(start_addr_sdram),	// 25:0
+	.finish_address_sdram	(finish_addr_sdram),	// 25:0
 	.rowCache_address_sram	(),	// 25:0
 	.output_address_sram	(),	// 25:0
 
-	.sdram_address			(),	// 25:0
-	.sram_address			()	// 25:0
+	.sdram_address			(address_sdram),	// 25:0
+	.sram_address			(address_sram)		// 25:0
 );
 
 // control unit module
 controlUnit thalai
 (
-	.clk					(),
-	.n_rst					(),
-	.start_flag				(),
-	.dataRead_sram			(),
-	.dataRead_sdram			(),
-	.rollover_i				(),
-	.rollover_j				(),
-	.rollover_i_wr			(),
+	.clk					(clk),
+	.n_rst					(n_rst),
+	.start_flag				(start_flag),
+	.dataRead_sram			(sram_datareadvalid),
+	.dataRead_sdram			(sdram_datareadvalid),
+	.rollover_i				(rollover_i),
+	.rollover_j				(rollover_j),
+	.rollover_i_wr			(rollover_i_wr),
 	
-	.enable_i				(),
-	.enable_j				(),
-	.enable_i_wr			(),
-	.enable_addr_calc_sram	(),
-	.enable_addr_calc_sdram	(),
-	.enable_WB				(),
-	.enable_sram			(),
-	.read_en_sdram			(),
-	.write_en_sdram			(),
-	// output modes below
-	.mode_addr_calc_sram	(),
-	.mode_addr_calc_sdram	(),
-	.mode_WB				(),	// 2:0
-	.mode_sram				(),
-	// finish flag below
-	.finish_flag			()
+	.enable_i				(i_en),
+	.enable_j				(j_en),
+	.enable_i_wr			(i_wr_en),
+	.enable_addr_calc_sram	(addrCalc_sram_en),
+	.enable_addr_calc_sdram	(addrCalc_sdram_en),
+	.enable_WB				(WB_en),
+	.enable_sram			(sram_en),
+	.read_en_sdram			(sdram_read_en),
+	.write_en_sdram			(sdram_write_en),
+	// modes below
+	.mode_addr_calc_sram	(addrCalc_sram_mode),
+	.mode_addr_calc_sdram	(addrCalc_sdram_mode),
+	.mode_WB				(mode_WB),	// 2:0
+	.mode_sram				(mode_sram),
+	.finish_flag			(finish_flag)
 );
 
 // Window buffer module
 wbuffer windowBuffer
 (
-	.clk					(),
-   	.nrst					(),
-   	.enable_CU				(),
-   	.mode					(),	// 2:0
+	.clk					(clk),
+   	.nrst					(n_rst),
+   	.enable_CU				(WB_en),
+   	.mode					(mode_WB),	// 2:0
    	.data_read				(),	// 7:0
    	.data					(),	// 7:0
 
-   	.w_1					(),	// 7:0
-   	.w_2					(),	// 7:0
-   	.w_3					(),	// 7:0
-   	.w_4					()	// 7:0
+   	.w_1					(wb1),	// 7:0
+   	.w_2					(wb2),	// 7:0
+   	.w_3					(wb3),	// 7:0
+   	.w_4					(wb4)	// 7:0
 );
 
 // RGGB re-ordering module
 rggb reorderingModule
 (
-	.wb_1					(),	// 7:0
-   	.wb_2					(),	// 7:0
-   	.wb_3					(),	// 7:0
-   	.wb_4					(),	// 7:0
-   	.row					(),
-   	.col					(),
+	.wb_1					(wb1),	// 7:0
+   	.wb_2					(wb2),	// 7:0
+   	.wb_3					(wb3),	// 7:0
+   	.wb_4					(wb4),	// 7:0
+   	.row					(j_value[0]),
+   	.col					(i_value[0]),
 
-	.out					()	// 31:0
+	.out					(reorderedWire)	// 31:0
 );
 
 // filter Top Level module
 filterTopLevel
 (
-	.clk					(),
-	.n_rst					(),
-	.[31:0] in				(),		// 31:0
-	.[1:0] filterMode		(),		// 1:0 	// from PCIe status registers
-	.[7:0] brightnessCoeff	(),		// 7:0
+	.clk					(clk),
+	.n_rst					(n_rst),
+	.[31:0] in				(reorderedWire),		// 31:0
+	.[1:0] filterMode		(filterMode),		// 1:0 	// from PCIe status registers
+	.[7:0] brightnessCoeff	(betaValue),		// 7:0
 	
-	.[31:0] result			()		// 31:0
+	.[31:0] result			(postFilterData)		// 31:0
 );
 
-module sram_simulation
+sram_simulation ennodaSRAM_simulation
 (
-	.clk					(),
+	.clk					(clk),
 	.sdram_data				(),	// 31:0
-	.wb_data				(),	// 31:0
-	.address				(),	// 25:0	
-	.mode					(),	// read/write
-	.addrCalcMode			(),
-	.enable					(),
+	.wb_data				(postFilterData),		// 31:0
+	.address				(address_sram),			// 25:0	
+	.mode					(mode_sram),			// read/write
+	.addrCalcMode			(addrCalc_sram_mode),
+	.enable					(sram_en),
 
 	.out_data				(),	// 31:0
-	.dataReadValid			()
+	.dataReadValid			(sraM_datareadvalid)
 );
 
 endmodule
