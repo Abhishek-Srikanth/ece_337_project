308fdf7852d314fdc012bcd5875657eb36104be5
Abhishek-Srikanth <srikanth@purdue.edu>
2015-12-14 00:50:26 -0500

    major changes made. to compile and test

--- source/brightnessFilter.sv
+++ source/brightnessFilter.sv
@@ -1,10 +1,10 @@
 // $Id: $
-// File name:   cont_bright.sv
+// File name:   brightnessFilter.sv
 // Created:     12/13/2015
-// Author:      Nikhil Ghanta
-// Lab Section: 337-04
-// Version:     1.1 Style change
-// Description: contrast and brightness
+// Author:      Abhishek Srikanth
+// Lab Section: 337-02
+// Version:     2.0 Major code change
+// Description: brightness only
 
 /*
 * To check if clock in b/w is required
@@ -15,31 +15,25 @@
 * Floating point multiplication is a complication we don't need right now
 */
 
-module cont_bright
+module brightnessFilter
 (
 	input wire [31:0] in,
    	//input wire alpha,
-   	input wire beta,
+   	input wire [7:0] beta,
    	output wire [31:0] result
 );
 
-   	wire [7:0] 	     pix_1;
-   	wire [7:0] 	     pix_2;
-   	wire [7:0] 	     pix_3;
-   	wire [7:0] 	     pix_4;
-   	assign pix_1 = in[31:24];
-   	assign pix_2 = in[23:16];
-   	assign pix_3 = in[15:8];
-   	assign pix_4 = in[7:0];
+   	wire [8:0]	red;
+   	wire [8:0]	green;
+   	wire [8:0]	blue;
+	assign red 		= in[31:24] + beta;  
+ 	assign green	= in[23:16] + beta;
+	assign blue		= in[7:0] + beta;
+	//assign result = {8'hFF, red, green, blue};
+	// NOTE: switch to above if timing is an issue
+	assign result = { 8'hFF,( red[8] == 1'b1 ? 8'hFF : red[7:0] ) ,  
+							( green[8] == 1'b1 ? 8'hFF : green[7:0] ) ,  
+							( blue[8] == 1'b1 ? 8'hFF : blue[7:0] ) 
+					}
 
-   	wire [7:0] 	     pix1_1;
-   	wire [7:0] 	     pix2_1;
-   	wire [7:0] 	     pix3_1;
-   	wire [7:0] 	     pix4_1;
-	assign pix1_1 = /*alpha */ pix_1 + beta;  
- 	assign pix2_1 = /*alpha */ pix_2 + beta;  
-	assign pix3_1 = /*alpha */ pix_3 + beta;  
-	assign pix4_1 = /*alpha */ pix_4 + beta;  
-	assign result = {pix1_1, pix2_1, pix3_1, pix4_1};
-
-endmodule // cont_bright
+endmodule
