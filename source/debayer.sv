5873f25e4396c373027a75bc1759aeadc55720c8
Tiger Cheng <Swaedo@users.noreply.github.com>
2015-12-15 08:19:07 -0500

    remove merge artifact

--- source/debayer.sv
+++ source/debayer.sv
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-module debayer
-  (
-   input wire [7:0] R,
-   input wire [7:0] G1,
-   input wire [7:0] G2,
-   input wire [7:0] B,
-   output reg [31:0] ARGB
-   );
-
-   reg [8:0] G_sum;
-   reg [7:0] G_average;
-   
-   always_comb
-   begin: DEBAYER
-    G_sum = G1 + G2;
-    G_average = G_sum[8:1];
-    ARGB = {8'd255, R, G_average, B};
-   end 
-
-endmodule 
-=======
 // $Id: $
 // File name:   debayer.sv
 // Created:     12/13/2015
@@ -49,4 +27,3 @@ assign out = { 8'hFF, in[31:24], green_sum[8:1] , in[7:0] };
 // change testBench if change made here
 
 endmodule
->>>>>>> origin/master
